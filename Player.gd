7cac37b034b8083f2f118abd2e65374d1f2d6f72
Samuel <ssoderbe@kth.se>
2023-09-29 22:19:36 +0200

    Player movement

--- Player.gd
+++ Player.gd
@@ -1,20 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
 
 
 func _physics_process(delta):
 
-
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	
+	if direction_x || direction_y:
+		velocity.x = direction_x * SPEED
+		velocity.y = direction_y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
