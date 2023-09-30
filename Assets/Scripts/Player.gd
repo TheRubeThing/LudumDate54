4a3b40b3e117ebeeac6aebfc981664a50b87ff0c
TheRubeThing <therubething@gmail.com>
2023-09-30 23:58:27 +0200

    Fix Player?

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
 @export var health: Resource
-@export var push_back_factor: int
+@export var push_back_factor: int = 100
+@export var pushback_decay: float = 0.8
 
 const SPEED = 100.0
 
 var equipped_item_instance: Item
-
-func _ready():
-	pass
+var pushback_velocity = Vector2(0,0)
 
 func equip_item(item: PackedScene) -> bool :
 	if (equipped_item_instance == null):
@@ -36,10 +35,16 @@ func _process(_delta):
 	if Input.is_action_just_pressed("throw"):
 		unequip_item()
 
-func _physics_process(delta):
+func _ready():
+	Globals.player = self
 
+
+func _physics_process(delta):
 	var direction_x = Input.get_axis("A_left", "D_right")
 	var direction_y = Input.get_axis("W_up", "S_down")
+	
+	
+	pushback_velocity *= pushback_decay
 
 	if direction_x || direction_y:
 		velocity.x = direction_x
@@ -50,18 +55,16 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		$Sprite.idle()
 	
-	velocity = velocity.normalized() * SPEED
+	velocity = (velocity.normalized() * SPEED) + pushback_velocity
 
 	move_and_slide()
 
 
 func _recoil(dir, amount):
-	# print(dir)
+	print(dir)
 	pushback_velocity = dir * amount * push_back_factor
 
 
-func _on_player_hit_box_hit(dmg, dmg_pos):
-	health.take_damage(dmg)
+func _on_hit_box_hit(dmg, dmg_pos):
 	var recoil_dir = (position - dmg_pos).normalized()
 	_recoil(recoil_dir, dmg)
-	
