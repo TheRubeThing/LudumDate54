19434f25cd7e52b7030599dc4ef564994d66452c
TheRubeThing <therubething@gmail.com>
2023-09-30 20:02:02 +0200

    Changes

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -10,10 +10,14 @@ func _physics_process(delta):
 	var direction_y = Input.get_axis("W_up", "S_down")
 
 	if direction_x || direction_y:
-		velocity.x = direction_x * SPEED
-		velocity.y = direction_y * SPEED
+		velocity.x = direction_x
+		velocity.y = direction_y
+		$Sprite.walk()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		$Sprite.idle()
+	
+	velocity = velocity.normalized() * SPEED
 
 	move_and_slide()
