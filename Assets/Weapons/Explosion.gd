f55311d2727cdb935dfefd0a0bef613294d1369b
TheRubeThing <therubething@gmail.com>
2023-10-01 17:39:06 +0200

    Splat sound

--- Assets/Weapons/Explosion.gd
+++ Assets/Weapons/Explosion.gd
@@ -5,6 +5,7 @@ var dmg = 20
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
+	$AnimationPlayer.play("Explosion")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
