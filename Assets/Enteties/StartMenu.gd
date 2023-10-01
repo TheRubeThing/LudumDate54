ad35871a8c8ce658d5e46600ce3c15475fcb0be6
Samuel <ssoderbe@kth.se>
2023-10-01 13:52:09 +0200

    stuff

--- Assets/Enteties/StartMenu.gd
+++ Assets/Enteties/StartMenu.gd
@@ -1,17 +1,11 @@
-extends CanvasLayer
+extends MarginContainer
 
-enum modes { GAME_RUNNING, START_MENU, PAUSE_MENU, GAME_OVER }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_screen()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func start_screen():
-	Globals.set_mode(modes.START_MENU)
-	Globals.pause_game()
--- Assets/Scripts/Global.gd
+++ Assets/Scripts/Global.gd
@@ -1,5 +1,8 @@
 extends Node
 
+enum modes { GAME_RUNNING, START_MENU, PAUSE_MENU, GAME_OVER }
+
+var current_mode : modes
 var player
 var player_stats
 
@@ -11,3 +14,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func set_mode(newMode):
+	current_mode = newMode
+	
+	
+func pause_game():
+	get_tree().paused = true
+	
+	
+func unpause_game():
+	get_tree().paused = false
