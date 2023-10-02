61410f7ba7bb2ba280fca5133d8b8904a2d2e38d
TheRubeThing <therubething@gmail.com>
2023-10-02 11:51:23 +0200

    Show mouse in menus

--- Assets/Scripts/Global.gd
+++ Assets/Scripts/Global.gd
@@ -7,6 +7,7 @@ signal score_updated(new_score)
 
 var current_mode : modes
 var player
+var cursor
 var player_stats
 var _score: int = 0
 
@@ -20,21 +21,25 @@ func _process(delta):
 
 func start_menu():
 	set_mode(modes.START_MENU)
+	show_mouse()
 	pause_game()
 	
 	
 func start_game():
 	set_mode(modes.GAME_RUNNING)
+	hide_mouse()
 	score_updated.emit(_score)
 	unpause_game()
 
 
 func game_over():
+	show_mouse()
 	set_mode(modes.GAME_OVER)
 	pause_game()
 	
 	
 func pause_menu():
+	show_mouse()
 	set_mode(modes.PAUSE_MENU)
 	pause_game()
 	
@@ -68,3 +73,12 @@ func add_to_score(score: int):
 	_score += score
 	score_updated.emit(_score)
 	
+func show_mouse():
+	if self.cursor:
+		self.cursor.visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func hide_mouse():
+	if self.cursor:
+		self.cursor.visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
